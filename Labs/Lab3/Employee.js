c3e01d2792472802aacf2e4d63489092b29e6748
Vinayaka98 <polimeravinayaka@gmail.com>
2022-09-25 20:34:25 -0400

    Successfully Added

---
+++ Labs/Lab3/Employee.js
@@ -0,0 +1,10 @@
+//TODO - Create Employee Module here and export to use in index.js
+
+let employees = [
+    {id: 1, firstName: "Pritesh", lastName: "Patel", email: "pritesh@mail.com", Salary:5000},
+    {id: 2, firstName: "Krish", lastName: "Lee", email: "krish@mail.com", Salary:4000},
+    {id: 3, firstName: "Racks", lastName: "Jacson", email: "racks@mail.com", Salary:5500},
+    {id: 4, firstName: "Denial", lastName: "Roast", email: "denial@mail.com", Salary:9000}
+]
+
+module.exports = employees;
